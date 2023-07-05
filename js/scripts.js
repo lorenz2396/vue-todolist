25c53fd99ef02ced2533da4af15ceff3b1394659
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-07-05 11:20:55 +0200

    milestones-done

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,47 @@ const { createApp } = Vue;
 createApp ({
     data(){
         return {
-            
+            toDoTitle : "Che cosa vuoi comprare?",
+            newTodo : {
+                text: '',
+                done: false
+            },
+            todos: [
+                {
+                    text:'Uova',
+                    done: true
+                },
+                {
+                    text:'Affettati',
+                    done: false
+                },
+                {
+                    text:'Latte',
+                    done: false
+                }
+            ]
         }
     },
     methods: {
-        
+        addTodo(){
+            console.log('Cliccato su aggungi', this.newTodo.text);
+            const newTodoCreato = {
+                text: this.newTodo.text,
+                done: false
+                // o ...this.newTodo   x creare una nuova copia esplicita
+            }
+            this.todos.push(newTodoCreato);
+            
+            this.newTodo.text = '';
+        },
+        removeToDo(index){
+            console.log('Cliccato su X', index);
+            this.todos.splice(index, 1);
+        },
+        checkTastiera(){
+            console.log(this.newTodo.text)
+        }
     },
 
 }).mount('#app');
+
